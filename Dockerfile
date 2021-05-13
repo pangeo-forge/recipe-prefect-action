602077638730adb55b2d427f3afb3e86f9496a5c
sharkinsspatial <sharkinsgis@gmail.com>
2021-05-13 17:23:18 -0500

    Include version checking for action and environment before registration.

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,12 @@ COPY ./requirements.txt requirements.txt
 RUN conda run -n notebook pip uninstall -y -r requirements.txt
 RUN conda run -n notebook pip install -I -r requirements.txt
 
-RUN pip install git+https://github.com/pangeo-forge/pangeo-forge-prefect@0.1
+RUN pip install git+https://github.com/pangeo-forge/pangeo-forge-prefect@0.2
 COPY action/process_recipe.py /process_recipe.py
 COPY entrypoint.sh /entrypoint.sh
+
+ENV PANGEO_NOTEBOOK_VERSION=2021.05.04
+ENV PANGEO_FORGE_RECIPES_VERSION=0.3.3
+ENV PREFECT_VERSION=0.14.7
 
 ENTRYPOINT [ "sh", "/entrypoint.sh" ]
--- action/process_recipe.py
+++ action/process_recipe.py
@@ -2,13 +2,19 @@ import sys
 import os
 import json
 from pangeo_forge_prefect.flow_manager import register_flow
+from pangeo_forge_prefect.meta_types.versions import Versions
 
 def main():
     secrets = json.loads(os.environ["SECRETS_CONTEXT"])
     print(secrets)
     meta_yaml = os.environ["INPUT_PATH_TO_META_YAML"]
     bakeries_yaml = os.environ["INPUT_PATH_TO_BAKERIES_YAML"]
-    register_flow(meta_yaml, bakeries_yaml, secrets)
+    versions = Versions(
+        pangeo_notebook_version=os.environ["PANGEO_NOTEBOOK_VERSION"],
+        pangeo_forge_version=os.environ["PANGEO_FORGE_RECIPES_VERSION"],
+        prefect_version=os.environ["PREFECT_VERSION"],
+    )
+    register_flow(meta_yaml, bakeries_yaml, secrets, versions)
 
 if __name__ == "__main__":
     main()
