f7e51f6600716fafc5354eca046781910916436e
sharkinsspatial <sharkinsgis@gmail.com>
2021-05-20 16:47:25 -0500

    Do not run command in conda environment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 # Process dependencies here that need installing - Assuming that the recipe either has:
 # - Conda environment .yaml file attached
 # - Dependencies in a list in the meta.yaml
-conda run --no-capture-output -n notebook python3 /process_recipe.py
+python3 /process_recipe.py
