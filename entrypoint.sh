63cab7022f0f3f4e82786bd94db012014e980315
sharkinsspatial <sharkinsgis@gmail.com>
2021-05-20 20:30:34 -0500

    Restrict modules installed via pip for pangeo_forge_prefect.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 # Process dependencies here that need installing - Assuming that the recipe either has:
 # - Conda environment .yaml file attached
 # - Dependencies in a list in the meta.yaml
-pip list
 conda run --no-capture-output -n notebook python3 /process_recipe.py
