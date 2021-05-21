ef394a56bb25710d9f65ff9ed269518ba5a65feb
sharkinsspatial <sharkinsgis@gmail.com>
2021-05-20 19:02:19 -0500

    Diagnosing xarray versioning issues.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 # Process dependencies here that need installing - Assuming that the recipe either has:
 # - Conda environment .yaml file attached
 # - Dependencies in a list in the meta.yaml
-python3 /process_recipe.py
+pip list
+conda run --no-capture-output -n notebook python3 /process_recipe.py
